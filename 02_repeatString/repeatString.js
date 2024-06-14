83e3d7bb7f2bd19c1c0b5cf4519865a26e3cdaf3
Hillary Adams <hradams4@asu.edu>
2024-06-14 17:29:53 -0400

    Complete testing of repeatString function with all tests passes

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let resultString = "";
+    if (num >= 0){
+        for (let i = 1; i <= num; i++){
+            resultString = resultString + string;
+        }
 
+        return resultString;
+        resultString = "";
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
