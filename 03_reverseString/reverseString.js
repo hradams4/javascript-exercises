48458b461ca28e6f795079cec4b3ce6b74dff5de
Hillary Adams <hradams4@asu.edu>
2024-06-16 23:41:53 -0400

    Complete reverseString function to pass all tests succesfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
 
+
+const reverseString = function(string) {
+    let reversedString = "";
+    let stringArray = string.split("");
+    let reversedStringArray = [];
+
+    for(let i = stringArray.length; i >= 0; i--){
+        reversedStringArray.push(stringArray[i]);
+    }
+     
+    reversedString = reversedStringArray.join("");
+
+    return reversedString;
 };
 
 // Do not edit below this line
