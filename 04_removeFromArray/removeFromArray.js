a46cca32b5114e5a5edce3d94690962071a7a26f
Hillary Adams <hradams4@asu.edu>
2024-06-17 20:01:46 -0400

    Complete removeFromArray function to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value) {
+    let args = Array.from(arguments);
+    let result = [];
+
+
+    for(let i = 0; i < array.length; i++){
+        for(let arg = 1; arg < args.length; arg++){
+        
+            if(array[i] === args[arg]){
+                delete array[i];
+            }
+        }
+    }
+    result = array.filter(Boolean);
+    return result;
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
