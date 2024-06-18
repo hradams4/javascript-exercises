8e3b5613ee859653c1d40c6b7fcd1a5cd36979d6
Hillary Adams <hradams4@asu.edu>
2024-06-18 00:02:53 -0400

    Complete both coversion methods to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+let celsius = 0;
+let farenheit = 0;
+
+const convertToCelsius = function(fTemp) {
+
+  celsius = (fTemp - 32) * (5/9);
+
+  return Math.round(celsius * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+
+  farenheit = (cTemp * (9/5) + 32);
+
+  return Math.round(farenheit * 10) / 10;
+
 };
 
 // Do not edit below this line
