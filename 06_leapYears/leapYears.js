956cd5f234fff713f5cc05050ef95763374ef0eb
Hillary Adams <hradams4@asu.edu>
2024-06-17 23:20:47 -0400

    Complete leapYears function to pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,24 @@
-const leapYears = function() {
+//year % 4 = true unless
+//year % 100 unless also % 400
+
+const leapYears = function(year) {
+    let isLeapYear;
+
+    if(year % 4 === 0){
+    
+        if(year % 100 === 0 && year % 400 === 0){
+            isLeapYear = true;
+        } else if(year % 100 === 0 && year % 400 !== 0){
+            isLeapYear = false;
+        } else {
+            isLeapYear = true;
+        }
+
+    } else {
+        isLeapYear = false;
+    }
+
+    return isLeapYear;
 
 };
 
